1fa205ef7866c737f037e1ddc32070cce60b7d5a
Ian Bazán <maysian21@gmail.com>
2025-04-30 20:14:11 -0500

    crendciasles aggarara me esta

--- proyecto-carrito-front/src/pages/Comprar.tsx
+++ proyecto-carrito-front/src/pages/Comprar.tsx
@@ -40,6 +40,7 @@ const Comprar: React.FC = () => {
           headers: {
             "Content-Type": "application/json",
           },
+          credentials: "include", 
           body: JSON.stringify(body),
         }
       );
